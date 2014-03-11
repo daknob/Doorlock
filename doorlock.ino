6301a7eeb0c11893a7f96b661f0a29855500bccf
Foivos S. Zakkak <foivos@zakkak.net>
2014-03-12 01:30:52 +0200

    Energy saving implementation without state

--- doorlock.ino
+++ doorlock.ino
@@ -23,123 +23,93 @@ static const int Switch = 6; /* The pin for the switch inside ToLABaki */
  *********************************************/
 
 /* Define some easy names */
-#define OPEN       1
-#define CLOSED     0
-#define CLOCKWISE  1
-#define CCLOCKWISE 0
+#define CLOCKWISE  LOW
 
 /* Declare all variables */
-int state = CLOSED;  /* State of the door */
 int sBuff = 0;       /* Serial Buffer */
 
 /* Set appropriate pin modes */
 void setup(){
+
     pinMode(Step, OUTPUT);
     pinMode(Dir, OUTPUT);
     pinMode(Error, OUTPUT);
     pinMode(Switch, INPUT);
     pinMode(Enable, OUTPUT);
-    digitalWrite(Enable, LOW);
+    /* start with the stepper driver disabled */
+    digitalWrite(Enable, HIGH);
+    digitalWrite(Dir, CLOCKWISE);
     /* Initialize Serial connection with a computer */
     Serial.begin(9600);
     delay(100);
+
 }
 
 /* Run the actual code to use sensors for open and close */
 void loop(){
+
     if (Serial.available() > 0){ /* Full Sensoring */
         sBuff = Serial.read();   /* Read one byte from the Serial port */
         if(sBuff == SerialOpen){
-            if(openDoor()){
-                Serial.write(SerialOk);
-            }else{
-                Serial.write(SerialNo);
-            }
+            openDoor();
+            Serial.write(SerialOk);
         }else{
             Serial.write(SerialNack);
         }
     }
-    if(digitalRead(Switch) == HIGH){  /* Door opens using internal switch */
+
+    /* Open the door using the internal switch */
+    if(digitalRead(Switch) == HIGH){
         openDoor();
     }
+
 }
 
 /* Opens door if not open */
-int openDoor(){
-    if(state==OPEN){ /* Already open */
-        displayError();
-        return 0;
-    }else{
-        state = OPEN;
-        /* We turn the gear clockwise, so the other gear spins counterclockwise */
-        rotateMotor(CLOCKWISE);
-        return 1;
-    }
-}
+void openDoor(){
 
-/* Closes door if not closed */
-int closeDoor(){
-    if(state==CLOSED){ /* Already closed */
-        displayError();
-        return 0;
-    }else{
-        state = CLOSED;
-        /* We turn the gear cclockwise, so the other gear spins clockwise */
-        rotateMotor(CCLOCKWISE);
-        return 1;
-    }
-}
+    /* Enable the stepper motor driver */
+    digitalWrite(Enable, LOW);
+    /* give it some time to boot */
+    delay(1700); /* Normally it takes 1600, but
+                  * just to be on the safe side */
 
-/* Changes the door lock state */
-void toggleDoor(){
-    if(state==CLOSED){
-        openDoor();
-    }else{
-        closeDoor();
-    }
-    return;
-}
+    /* We turn the gear clockwise, so the other gear spins
+     * counterclockwise */
+    sendRotatePulse();
 
-/* Force a door state; Use with caution */
-void forceDoorState(int st){
-    state = st;
-    if(st == CLOSED){
-        closeDoor();
-    }else{
-        openDoor();
-    }
-    return;
+    /* keep it open for one second */
+    delay(1000);
+    /* Disable the stepper motor driver, the handle spring should
+     * bring it to it's normal state. */
+    digitalWrite(Enable, HIGH);
+
 }
 
 /* Display an 1 second error blink on the Error LED */
 void displayError(){
-    for(int i=1;i<=10;i++){
+    int i;
+
+    for(i=1; i<=10; i++) {
         digitalWrite(Error, HIGH);
         delay(100);
         digitalWrite(Error, LOW);
         delay(100);
     }
+
 }
 
 /* Spin the motor a known amount of times */
-void rotateMotor(int direction){
-    if(direction == CLOCKWISE){
-        digitalWrite(Dir, LOW);
-        sendRotatePulse();
-    }else{
-        digitalWrite(Dir, HIGH);
-        sendRotatePulse();
-    }
-}
-
 void sendRotatePulse(){
-    for(int i=1;i<=StepsToRotate;i++){
+    int i;
+
+    for(i=1; i<=StepsToRotate; i++) {
         digitalWrite(Step, HIGH);
         delay(1);
         digitalWrite(Step, LOW);
         delay(1);
     }
-    return;
+
 }
 
 /* Local Variables: */
